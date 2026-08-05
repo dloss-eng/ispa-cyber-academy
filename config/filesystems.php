58c8fec718fcf1e218ab1e9831fa393cc6bb47e2
loss <loss@gmail.com>
2026-08-05 16:57:53 +0000

    feat: ajout du disque cloudinary

--- config/filesystems.php
+++ config/filesystems.php
@@ -4,6 +4,9 @@ return [
     'disks' => [
         'local' => ['driver'=>'local','root'=>storage_path('app/private'),'serve'=>true,'throw'=>false],
         'public' => ['driver'=>'local','root'=>storage_path('app/public'),'url'=>env('APP_URL').'/storage','visibility'=>'public','throw'=>false],
+        'cloudinary' => [
+            'driver' => 'cloudinary',
+        ],
     ],
     'links' => [public_path('storage') => storage_path('app/public')],
 ];
